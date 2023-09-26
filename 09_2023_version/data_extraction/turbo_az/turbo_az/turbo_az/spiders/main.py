bda6febe2aeb1d7825dd0617b86db0b3fc82ec7d
Səmədov İsmət <ismetsemedov@gmail.com>
2023-09-26 19:02:59 +0400

    check

--- 09_2023_version/data_extraction/turbo_az/turbo_az/turbo_az/spiders/main.py
+++ 09_2023_version/data_extraction/turbo_az/turbo_az/turbo_az/spiders/main.py
@@ -1,14 +1,26 @@
 import scrapy
 
-
 class MainSpider(scrapy.Spider):
     name = "main"
     allowed_domains = ["turbo.az"]
-    start_urls = ["https://turbo.az/autos/7267410-geely-tugella-s"]
+    start_urls = ["https://turbo.az/autos?page=1"]
 
     def parse(self, response):
-        yield from self.parse_car_details(response)
+        hrefs = response.css('.products-i__link::attr(href)').getall()
+        for href in hrefs:
+            yield scrapy.Request(
+                url=response.urljoin(href),
+                callback=self.parse_car_details,
+                meta={"href": href}
+            )
 
+        # Check for the "Next" button using the "rel" attribute
+        next_button = response.css('a[rel="next"]::attr(href)').get()
+        if next_button:
+            yield scrapy.Request(
+                url=response.urljoin(next_button),
+                callback=self.parse
+            )
     def parse_car_details(self, response):
         yield {
             'title'     : response.css('h1.product-title::text').get(),
@@ -20,3 +32,4 @@ class MainSpider(scrapy.Spider):
             'update'    : response.css('span.product-statistics__i-text::text').get(),
             'views'     : response.css('ul.product-statistics li:nth-child(2) span::text').get(),
         }
+
