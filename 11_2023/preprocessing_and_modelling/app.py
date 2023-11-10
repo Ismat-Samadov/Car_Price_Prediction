7ac9ef2228116a103e9d04750a6c3c8626d611c7
Ismat-Samadov <ismetsemedov@gmail.com>
2023-11-10 19:00:29 +0400

    check

--- 11_2023/preprocessing_and_modelling/app.py
+++ 11_2023/preprocessing_and_modelling/app.py
@@ -1,52 +1,71 @@
 import streamlit as st
 import joblib
 import pandas as pd
-xgboost_model = joblib.load('xgboost_model.pkl')
-datacleaned = pd.read_excel('datacleaned.xlsx')
+from sklearn.preprocessing import OneHotEncoder
 
-car_image_url = "https://images.unsplash.com/photo-1511919884226-fd3cad34687c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGNhcnxlbnwwfHwwfHx8MA%3D%3D"
-image_html = f'<img src="{car_image_url}" style="max-width: 100%; height: auto;">'
-st.markdown(image_html, unsafe_allow_html=True)
-hide_st_style = """
-<style>
-#MainMenu {visibility: hidden;}
-footer {visibility: hidden;}
-header {visibility: hidden;}
-</style>
-"""
-st.markdown(hide_st_style, unsafe_allow_html=True)
-st.sidebar.title("Input Parametres")
-st.title("Car Price Prediction App")
-city = st.sidebar.selectbox("Select which city you want to buy:",datacleaned['city'].unique())
-make = st.sidebar.selectbox("Select which make you want to buy",datacleaned['make'].unique())
-model = st.sidebar.selectbox("Select which model you want to buy",datacleaned['model'].unique())
-year = st.sidebar.selectbox("Select which year's car you want to buy",datacleaned['year'].unique())
-ban_type = st.sidebar.selectbox("Select which ban type car you want to buy",datacleaned['ban_type'].unique())
-colour = st.sidebar.selectbox("Select which colour car you want to buy",datacleaned['colour'].unique())
-engine_power = st.sidebar.selectbox("Select engine_power of car you want to buy",datacleaned['engine_power'].unique())
-ride_km = st.sidebar.slider("Select km of car you want to buy",0,5560000,50)
-transmission = st.sidebar.selectbox("Select transmission of car you want to buy", datacleaned['transmission'].unique())
-gear = st.sidebar.selectbox("Select gear of car you want to buy", datacleaned['gear'].unique())
-is_new = st.sidebar.selectbox("This car is new ?", ("Yes","No"))
+# Load the pre-trained XGBoost model
+model = joblib.load('xgboost_model.pkl')
 
-data = {
-    "city": city,
-    "make": make,
-    "model": model,
-    "year": year,
-    "ban_type": ban_type,
-    "colour": colour,
-    "engine_power": engine_power,
-    "ride_km":ride_km,
-    "transmission":transmission,
-    "gear":gear,
-    "is_new":1 if is_new == "Yes" else 0
-}
+categorical_columns = ['city', 'make', 'model', 'ban_type', 'colour', 'transmission', 'gear', 'is_new']
+numeric_columns = ['year', 'engine_power', 'ride_km']
+encoder = OneHotEncoder()
 
-columns = datacleaned.columns
-df = pd.DataFrame.from_dict([data])
-df = pd.get_dummies(df).reindex(columns=columns, fill_value=0)
+# Load your training data (assuming you have it saved or accessible)
+training_data = pd.read_excel('datacleaned.xlsx')  # Replace with your actual file path
 
-if st.sidebar.button('Predict Price'):
-    prediction = xgboost_model.predict(df)
-    st.success(f'Predicted Price: {prediction}')
+# Fit the OneHotEncoder on the training data
+encoder.fit(training_data[categorical_columns])
+
+def preprocess_input(data):
+    categorical_data = data[categorical_columns]
+    categorical_encoded = encoder.transform(categorical_data).toarray()
+    numeric_data = data[numeric_columns]
+    processed_data = pd.concat([pd.DataFrame(categorical_encoded, columns=encoder.get_feature_names_out(categorical_columns)),
+                               numeric_data], axis=1)
+
+    return processed_data
+
+def predict_price(features):
+    processed_features = preprocess_input(features)
+    prediction = model.predict(processed_features)
+    return prediction
+
+def main():
+    st.title("Car Price Prediction App")
+    st.sidebar.header("Input Features")
+    city = st.sidebar.text_input("City", "")
+    make = st.sidebar.text_input("Make", "")
+    model_input = st.sidebar.text_input("Model", "")
+    year = st.sidebar.number_input("Year", 1940, 2023, step=1)
+    ban_type = st.sidebar.selectbox("Body Type",
+                                    ["SUV", "Sedan", "Hatchback", "Station Wagon", "Liftback", "Truck", "Van",
+                                     "Minivan", "Coupe", "Motorcycle", "Pickup", "Convertible", "Microbus", "Moped",
+                                     "Bus", "Roadster", "Quad Bike"])
+    colour = st.sidebar.selectbox("Color",
+                                  ["Silver", "Black", "Blue", "Gray", "Brown", "Dark_Red", "Red", "Green", "Blue",
+                                   "Beige", "Gold", "Brown", "Orange", "Yellow", "Purple", "Pink"])
+    engine_power = st.sidebar.number_input("Engine Power", min_value=0.0)
+    ride_km = st.sidebar.number_input("Ride (km)", min_value=0.0)
+    transmission = st.sidebar.selectbox("Transmission", ["Automatic", "Manual", "CVT", "Automated_Manual"])
+    gear = st.sidebar.selectbox("Gear", ["Full", "Front", "Rear"])
+    is_new = st.sidebar.selectbox("Is New", ["Yes", "No"])
+
+    user_inputs = {'city': city, 'make': make, 'model': model_input, 'year': year, 'ban_type': ban_type,
+                   'colour': colour,
+                   'engine_power': engine_power, 'ride_km': ride_km, 'transmission': transmission, 'gear': gear,
+                   'is_new': is_new}
+    input_df = pd.DataFrame([user_inputs])
+    processed_input = preprocess_input(input_df)
+    st.write("Processed Input Data:")
+    st.write(processed_input)
+
+    if st.button("Predict Car Price"):
+        prediction = predict_price(processed_input)
+        st.success(f"Predicted Car Price: {prediction[0]:,.2f} AZN")
+
+        # Display additional information or visualization about the prediction
+        # Example: st.line_chart(prediction_history) or st.plotly_chart(prediction_chart)
+
+
+if __name__ == '__main__':
+    main()
