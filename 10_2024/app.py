df2142c8db058c0cc132abf798adc22f04b07c45
Ismat <ismetsemedov@gmail.com>
2024-10-10 18:09:08 +0400

    check

--- 10_2024/app.py
+++ 10_2024/app.py
@@ -52,5 +52,6 @@ def predict():
         return jsonify({'error': str(e)})
 
 # Run the app
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
